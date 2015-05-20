880d16e48d69090689dbafd73b10af99649b3e32
Alex Izotov <martinandrudy@gmail.com>
2015-05-20 13:12:47 +0300

    added StaticPages controller;
    added static_pages_spec.rb;
    added spork + guard
    
    Signed-off-by: Alex Izotov <martinandrudy@gmail.com>

---
+++ spec/features/static_pages_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+feature "Static pages" do
+
+  given(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  feature "Home page" do
+
+    scenario "should have the h1 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    scenario "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_title("#{base_title} | Home")
+    end
+  end
+
+  feature "Help page" do
+
+    scenario "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_css('h1', :text => 'Help')
+    end
+
+    scenario "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_title("#{base_title} | Help")
+    end
+  end
+
+  feature "About page" do
+
+    scenario "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    scenario "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_title("#{base_title} | About Us")
+    end
+  end
+
+  feature "Contact page" do
+
+    scenario "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    scenario "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_title("#{base_title} | Contact")
+    end
+  end
+end
