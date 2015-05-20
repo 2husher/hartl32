c666b9b2075b280cec191e487091a3c5becccbab
Alex Izotov <martinandrudy@gmail.com>
2015-05-20 14:44:06 +0300

    added html structure and bootstrap; shortened spec tests; added full_title spec helper
    Signed-off-by: Alex Izotov <martinandrudy@gmail.com>

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,63 +1,36 @@
 require 'spec_helper'
 
 feature "Static pages" do
+  subject { page }
 
   given(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   feature "Home page" do
+    before { visit root_path }
 
-    scenario "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
-    end
-
-    scenario "should have the base title" do
-      visit '/static_pages/home'
-      page.should have_title("Ruby on Rails Tutorial Sample App")
-    end
-
-    scenario "should not have a custom page title" do
-      visit '/static_pages/home'
-      page.should_not have_title('| Home')
-    end
+    scenario { should have_selector('h1', :text => 'Sample App') }
+    scenario { should have_title(full_title("")) }
+    scenario { should_not have_title('| Home') }
   end
 
   feature "Help page" do
+    before { visit help_path }
 
-    scenario "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_css('h1', :text => 'Help')
-    end
-
-    scenario "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_title("#{base_title} | Help")
-    end
+    scenario { should have_css('h1', :text => 'Help') }
+    scenario { should have_title(full_title("Help")) }
   end
 
   feature "About page" do
+    before { visit about_path }
 
-    scenario "should have the h1 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
-    end
-
-    scenario "should have the title 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_title("#{base_title} | About Us")
-    end
+    scenario { should have_selector('h1', :text => 'About Us') }
+    scenario { should have_title(full_title("About Us")) }
   end
 
   feature "Contact page" do
+    before { visit contact_path }
 
-    scenario "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', :text => 'Contact')
-    end
-
-    scenario "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_title("#{base_title} | Contact")
-    end
+    scenario { should have_selector('h1', :text => 'Contact') }
+    scenario { should have_title(full_title("Contact")) }
   end
 end
