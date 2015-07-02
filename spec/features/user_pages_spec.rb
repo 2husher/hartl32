2d5faf5aefae563a41977f5ee5c3352033585067
Alex Izotov <martinandrudy@gmail.com>
2015-07-02 15:11:36 +0300

    added showing microposts at user page
    Signed-off-by: Alex Izotov <martinandrudy@gmail.com>

--- spec/features/user_pages_spec.rb
+++ spec/features/user_pages_spec.rb
@@ -53,10 +53,19 @@ feature "User pages" do
 
   feature "profile page" do
     given(:user) { FactoryGirl.create(:user) }
+    given!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    given!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
     before { visit user_path(user) }
 
     scenario { should have_css('h1',    text: user.name) }
     scenario { should have_title(user.name) }
+
+    feature "microposts" do
+      scenario { should have_content(m1.content) }
+      scenario { should have_content(m2.content) }
+      scenario { should have_content(user.microposts.count) }
+    end
   end
 
   feature "signup" do
